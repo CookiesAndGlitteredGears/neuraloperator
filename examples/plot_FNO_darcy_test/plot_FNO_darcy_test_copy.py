1b6e5ca042507dfac43b1c1c3d5fbd3b2d803483
CookiesAndGlitteredGears <76476817+CookiesAndGlitteredGears@users.noreply.github.com>
2024-08-16 00:38:22 +0800

    added log file; need to see how to have log file and consoleoutput

--- examples/plot_FNO_darcy_test/plot_FNO_darcy_test_copy.py
+++ examples/plot_FNO_darcy_test/plot_FNO_darcy_test_copy.py
@@ -32,6 +32,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 current_date = datetime.datetime.now().strftime('%Y_%b_%d_%H_%M_%S')
 if not os.path.exists('./output'): os.mkdir('./output')
 if not os.path.exists(f'./output/{current_date}'): os.mkdir(f'./output/{current_date}')
+sys.stdout = open(f'./output/{current_date}/log_file.txt', 'w')
 
 def extract_boundary(nparray):
     array_dim = nparray.shape
@@ -195,7 +196,7 @@ def get_sensors_evenly_spaced(nparray, proportion = 0.25):
 # )
 train_resolution = 16
 
-test_resolution = 421
+test_resolution = 128
 train_loader, test_loaders, data_processor = load_darcy_flow_small(
         n_train=100, batch_size=32,
         train_resolution=train_resolution,
@@ -211,6 +212,48 @@ train_loader, test_loaders, data_processor = load_darcy_flow_small(
 train_loader.dataset.y, train_loader.dataset.x = train_loader.dataset.x, train_loader.dataset.y
 # test_loaders[16].dataset.y, test_loaders[16].dataset.x = test_loaders[16].dataset.x, test_loaders[16].dataset.y
 test_loaders[test_resolution].dataset.y, test_loaders[test_resolution].dataset.x = test_loaders[test_resolution].dataset.x, test_loaders[test_resolution].dataset.y
+
+# fig = plt.figure(figsize=(8, 8))
+#
+#
+# for index in range(3):
+#     data = train_loader[index]
+#     data = data_processor.preprocess(data, batched=False)
+#     # Input x
+#     x = data['x']
+#     # Ground-truth
+#     y = data['y']
+#     # Model prediction
+#
+#
+#     #send to cpu for plotting
+#     x, y = x.cpu(), y.cpu()
+#
+#     # print(out)
+#
+#     ax = fig.add_subplot(4, 4, index*4 + 1)
+#     ax.imshow(x[0], cmap='cubehelix')
+#     plot = ax.pcolor(x[0])
+#     fig.colorbar(plot)
+#     if index == 0:
+#         ax.set_title('Input x')
+#     plt.xticks([], [])
+#     plt.yticks([], [])
+#
+#     offset = 0.3
+#
+#     ax = fig.add_subplot(4, 4, index*4+ 2)
+#     ax.imshow(y.squeeze(),cmap='cubehelix')
+#     plot = ax.pcolor(y.squeeze(), vmin = min(y.squeeze().flatten())-offset, vmax = max(y.squeeze().flatten())+offset)
+#     fig.colorbar(plot)
+#     if index == 0:
+#         ax.set_title('Ground-truth y')
+#     plt.xticks([], [])
+#     plt.yticks([], [])
+
+
+
+
 
 # sig = plt.figure(figsize=(2, 2))
 # ax = sig.add_subplot(1,1)
@@ -259,13 +302,13 @@ data_processor = data_processor.to(device)
 #              in_channels=3,
 #              out_channels=1
 #              )
-model = TFNO(n_modes=(32, 32),
-             hidden_channels=128,
-             projection_channels=256,
+model = TFNO(n_modes=(16,16),
+             hidden_channels=32,
+             projection_channels=64,
              factorization='tucker',
              rank=0.42,
-             in_channels=3,
-             out_channels=1
+             # in_channels=3,
+             # out_channels=1
              )
 model = model.to(device)
 
@@ -278,7 +321,7 @@ sys.stdout.flush()
 
 # %%
 #Create the optimizer
-optimizer = torch.optim.SGD(model.parameters(),
+optimizer = torch.optim.AdamW(model.parameters(),
                                 lr=8e-3, 
                                 weight_decay=1e-4)
 scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
@@ -304,15 +347,14 @@ print(f'\n * Train: {train_loss}')
 print(f'\n * Test: {eval_losses}')
 sys.stdout.flush()
 
-3
 # %% 
 # Create the trainer
 trainer = Trainer(model=model,
-                  n_epochs=100,
+                  n_epochs=301,
                   device=device,
                   data_processor=data_processor,
                   wandb_log=False,
-                  log_test_interval=3,
+                  log_test_interval=10,
                   use_distributed=False,
                   verbose=True)
 
@@ -355,6 +397,7 @@ for index in range(3):
     # Ground-truth
     y = data['y']
     # Model prediction
+
     out = model(x.unsqueeze(0))
 
     #send to cpu for plotting
@@ -367,7 +410,7 @@ for index in range(3):
     plot = ax.pcolor(x[0])
     fig.colorbar(plot)
     if index == 0:
-        ax.set_title('Input x')
+        ax.set_title('Input u')
     plt.xticks([], [])
     plt.yticks([], [])
 
@@ -378,7 +421,7 @@ for index in range(3):
     plot = ax.pcolor(y.squeeze(), vmin = min(y.squeeze().flatten())-offset, vmax = max(y.squeeze().flatten())+offset)
     fig.colorbar(plot)
     if index == 0:
-        ax.set_title('Ground-truth y')
+        ax.set_title('Ground-truth a')
     plt.xticks([], [])
     plt.yticks([], [])
 
@@ -395,11 +438,11 @@ for index in range(3):
 
     ax = fig.add_subplot(4, 4, index*4 + 4)
     diff = y[0].squeeze()-out.squeeze().detach().numpy()
-    ax.imshow(diff, cmap='cubehelix')
+    ax.imshow(np.abs(diff), cmap='cubehelix')
     plot = ax.pcolor(diff, vmin = torch.min(diff)-offset, vmax =  torch.max(diff)+offset)
     fig.colorbar(plot)
     if index == 0:
-        ax.set_title('pred - truth')
+        ax.set_title('abs(pred - truth)')
     plt.xticks([], [])
     plt.yticks([], [])
 
@@ -420,6 +463,7 @@ plt.tight_layout()
 fig.show()
 fig.savefig(f'./output/{current_date}/fig.png')
 
+sys.stdout.close()
 
 # fig = plt.figure(figsize=(7, 7))
 # for index in range(3):
